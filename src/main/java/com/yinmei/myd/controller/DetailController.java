5f84c4dc1b7c225cd5ab399c4b5f5341718853ca
qiaochenxu <56715336+qiaochenxu@users.noreply.github.com>
2020-06-29 17:50:56 +0800

    修改展览

--- src/main/java/com/yinmei/myd/controller/DetailController.java
+++ src/main/java/com/yinmei/myd/controller/DetailController.java
@@ -19,7 +19,7 @@ public class DetailController extends Controller {
      */
     public void basicDetail(){
         String id = getPara("id","");
-        List<Record> list = Db.find("select  title,thumb,IFNULL(description,' ') description,FROM_UNIXTIME(inputtime) exhibitionTime from v9_jibenchenglie where status='99' and id=?", id);
+        List<Record> list = Db.find("select  j.title,j.thumb,d.zhengwen,FROM_UNIXTIME(j.inputtime) exhibitionTime from v9_jibenchenglie j,v9_jibenchenglie_data d where j.id=d.id and status='99' and j.id=?", id);
         set("list",list);
         renderJson();
     }
@@ -28,7 +28,7 @@ public class DetailController extends Controller {
      */
     public void foreShowDetail(){
         String id = getPara("id","");
-        List<Record> list = Db.find("select  title,thumb,IFNULL(description,' ') description,FROM_UNIXTIME(inputtime) exhibitionTime from v9_linshizhanlan where status='99' and id=?", id);
+        List<Record> list = Db.find("select  l.title,l.thumb,d.zhengwen,FROM_UNIXTIME(l.inputtime) exhibitionTime from v9_linshizhanlan l,v9_linshizhanlan_data d where l.id=d.id and status='99' and l.id=?", id);
         set("list",list);
         renderJson();
 
@@ -38,7 +38,7 @@ public class DetailController extends Controller {
      */
     public void  networkDetail(){
         String id = getPara("id","");
-        List<Record> list = Db.find("select  title,thumb,IFNULL(description,' ') description,FROM_UNIXTIME(inputtime) exhibitionTime from v9_wangshangzhanting where status='99' and id=?", id);
+        List<Record> list = Db.find("select  w.title,w.thumb,d.zhengwen,FROM_UNIXTIME(w.inputtime) exhibitionTime from v9_wangshangzhanting w,v9_wangshangzhanting_data d where w.id=d.id and status='99' and w.id=?", id);
         set("list",list);
         renderJson();
 
--- src/main/java/com/yinmei/myd/controller/ExhibitionController.java
+++ src/main/java/com/yinmei/myd/controller/ExhibitionController.java
@@ -22,7 +22,7 @@ public class ExhibitionController extends Controller {
         int pageNumber=getParaToInt(1,1);
         //每页显示数目，默认每页10条
         int pageSize = getParaToInt(2,10);
-        Page<Record> paginate = Db.paginate(pageNumber, pageSize, "select id,title,thumb,IFNULL(description,' ') description,FROM_UNIXTIME(inputtime) exhibitionTime", " from v9_jibenchenglie WHERE status ='99' ORDER BY exhibitionTime desc");
+        Page<Record> paginate = Db.paginate(pageNumber, pageSize, "select j.id,j.title,j.thumb,d.zhengwen,FROM_UNIXTIME(j.inputtime) exhibitionTime", " from v9_jibenchenglie j,v9_jibenchenglie_data d WHERE j.id=d.id and status ='99' ORDER BY exhibitionTime desc");
         set("paginate",paginate);
         renderJson();
 
@@ -48,7 +48,7 @@ public class ExhibitionController extends Controller {
         int pageNumber=getParaToInt(1,1);
         //每页显示数目，默认每页10条
         int pageSize = getParaToInt(2,10);
-        Page<Record> paginate = Db.paginate(pageNumber, pageSize, "select id,title,thumb,IFNULL(description,' ') description,FROM_UNIXTIME(inputtime) exhibitionTime", " from v9_wangshangzhanting WHERE status='99' ORDER BY exhibitionTime desc");
+        Page<Record> paginate = Db.paginate(pageNumber, pageSize, "select w.id,w.title,w.thumb,d.zhengwen,FROM_UNIXTIME(w.inputtime) exhibitionTime", " from v9_wangshangzhanting w,v9_wangshangzhanting_data d WHERE w.id=d.id and status='99' ORDER BY exhibitionTime desc");
         set("paginate",paginate);
         renderJson();
     }
